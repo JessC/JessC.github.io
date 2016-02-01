f70489b405871f9f5949af57d234eea13738c58a
JessC <jesse.callwood@gmail.com>
2016-01-31 21:31:12 -0800

    add preview window for projects

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,18 +1,63 @@
 $(window).load(function(){
 
-		$('#projects-container').css({visibility:'visible'});
 
-		$('#projects-container').masonry({
-			itemSelector: '.indiv-project:not(.filtered)',
-			//columnWidth:370,
-			isFitWidth: true,
-			isResizable: true,
-			isAnimated: !Modernizr.csstransitions,
-			gutterWidth: 25
-		});
+    // CREATE RANDOM COLORS ON PROJECT HOVER
+    $('.get-color').hover(function() {
+    	var randomcolor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+         //console.log(color); //SHOW RANDOM HEX COLOR
+         $('.hover-mask').css({background: [randomcolor]})
+       });
+    //END RANDOM COLORS FUNCTION
 
-		scrollSpyRefresh();
-		waypointsRefresh();
+
+    $('.indiv-project').click(function(e){
+    	e.preventDefault();
+
+    	var elem = $(this),
+    	image = elem.find('.project-image').html(),
+    	title = elem.find('.project-title').text(),
+    	descr = elem.find('.project-description').html(),
+    	elemDataCont = elem.find('.project-description');
+
+    	$('#project-title').text(title);
+    	$('#project-content').html(descr);
+    	$('#project-image').html(image);
+
+    	openProject();
+
+    });
+
+    function openProject(){
+
+		$('#project-preview').addClass('open');    //Makes prject div appear
+		$('.container').animate({'opacity':0},300);//Makes projects disappear
 		
-	});
+		setTimeout(function(){
+			$('#project-preview').slideDown();
+			$('.container').slideUp();	
+		},300);
+	}
 	
+	function closeProject(){
+
+		$('#project-preview').removeClass('open');
+		$('#project-preview').animate({'opacity':0},300);
+		
+		setTimeout(function(){
+			$('.container').slideDown();
+			$('#project-preview').slideUp();
+
+			$('.container').animate({'opacity':1},300);	
+		},300);
+		
+		// setTimeout(function(){
+		// 	$('#projects-container').masonry('reload');
+		// },500)
+}
+
+$('.close-preview').click(function(){
+	closeProject();
+})
+
+
+});
