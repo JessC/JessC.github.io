5faaabc4b7c7eb63d7314db934915fa755e09942
JessC <jesse.callwood@gmail.com>
2017-12-28 10:15:20 -0800

    change holiday to new year message

--- javascripts/barcode.js
+++ javascripts/barcode.js
@@ -104,11 +104,11 @@ $(function(){
     $('#randstation').click(function(){ 
         if  (hits % 2 !== 0) 
         {
-            JsBarcode("#barcode", randomWorkstation(listOfStations), {background: "#FF0000", text: "Holidays"});
+            JsBarcode("#barcode", randomWorkstation(listOfStations), {lineColor: "teal", background: "mistyrose", text: "Holidays"});
         }
         else
         { 
-            JsBarcode("#barcode", randomWorkstation(listOfStations), {background: "#00FF5D", text: "Happy"});
+            JsBarcode("#barcode", randomWorkstation(listOfStations), {lineColor: "purple", background: "#ccffff", text: "Happy"});
         }
         hits++;
         return false;
